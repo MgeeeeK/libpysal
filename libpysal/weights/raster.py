d0d583a3586506903022aa9d09e926d0a71c0562
MgeeeeK <40512095+MgeeeeK@users.noreply.github.com>
2020-07-17 21:01:04 +0530

    lazy weight

--- libpysal/weights/raster.py
+++ libpysal/weights/raster.py
@@ -3,6 +3,7 @@ from .weights import WSP
 import numpy as np
 import pandas as pd
 from warnings import warn
+from numba import njit
 
 try:
     from xarray import DataArray
@@ -12,7 +13,58 @@ except ImportError:
         "xarray must be installed to use this functionality")
 
 
-__all__ = ['da_checker', 'da2W', 'da2WSP', 'w2da']
+__all__ = ['da_checker', 'da2W', 'da2WSP', 'w2da', 'lazyWSP']
+
+
+class lazyWSP(object):
+    """
+    Prototype lazy wieghts contiguity class for raster interface.
+
+    Parameters
+    ----------
+    da : xarray.DataArray
+       raster file accessed using xarray.open_rasterio method
+    band : int
+       select band for da with multiple bands. Default is 1
+    criterion : {"rook", "queen"}
+       type of contiguity. Default is rook.
+
+    Attributes
+    ----------
+    n
+    index
+    neighbor_gen
+    id
+    id_nieghbors
+    """
+
+    def __init__(self, da, criterion, band=1):
+        band = da_checker(da, band)
+        da = da[band-1:band]
+        ser = da.to_series()
+        ids = np.where(ser != da.nodatavals[0])[0]
+        nodata = np.where(ser == da.nodatavals[0])[0]
+        props = {}
+        props["nrc"] = da.shape[1:]
+        n = props["nrc"][0] * props["nrc"][1]
+        narr = np.arange(n)
+        props["rid"] = np.floor_divide(narr, props["nrc"][1])
+        props["cid"] = np.mod(narr, props["nrc"][1])
+        props["mask"] = np.ones((n,), dtype=np.bool)
+        props["mask"][nodata] = False
+        ser = ser[ser != da.nodatavals[0]]
+        index = ser.index
+        self.index = index
+        self.n = len(index)
+        # generator yielding index and its neighbors
+        self.neighbor_gen = _lazybuilder(props, index, ids, criterion)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.id, self.id_nieghbors = next(self.neighbor_gen)
+        return self.id, self.id_nieghbors
 
 
 def da2W(da, criterion="rook", band=None, **kwargs):
@@ -145,3 +197,82 @@ def da_checker(da, band):
         band = 1
     da.attrs["shape"] = da.shape
     return band
+
+
+@njit
+def _getneighbor(i, nrows, ncols, rid, cid, ids, mask, criterion):
+    r1 = nrows - 1
+    c1 = ncols - 1
+    sp_n = np.zeros_like(ids, dtype=np.int8)
+    # top
+    if rid[i] > 0:
+        r = (rid[i] - 1) * ncols + cid[i]
+        if mask[r]:
+            r = find(ids, r)
+            sp_n[r] = 1
+    # left
+    if cid[i] > 0:
+        r = rid[i] * ncols + cid[i] - 1
+        if mask[r]:
+            r = find(ids, r)
+            sp_n[r] = 1
+    # right
+    if cid[i] < c1:
+        r = rid[i] * ncols + cid[i] + 1
+        if mask[r]:
+            r = find(ids, r)
+            sp_n[r] = 1
+    # below
+    if rid[i] < r1:
+        r = (rid[i] + 1) * ncols + cid[i]
+        if mask[r]:
+            r = find(ids, r)
+            sp_n[r] = 1
+    if criterion == "queen":
+        # northwest bishop
+        if cid[i] > 0 and rid[i] > 0:
+            r = (rid[i] - 1) * ncols - 1 + cid[i]
+            if mask[r]:
+                r = find(ids, r)
+                sp_n[r] = 1
+        # northeast bishop
+        if cid[i] < c1 and rid[i] > 0:
+            r = (rid[i] - 1) * ncols + 1 + cid[i]
+            if mask[r]:
+                r = find(ids, r)
+                sp_n[r] = 1
+        # southwest bishop
+        if cid[i] > 0 and rid[i] < r1:
+            r = (rid[i] + 1) * ncols - 1 + cid[i]
+            if mask[r]:
+                r = find(ids, r)
+                sp_n[r] = 1
+        # southeast bishop
+        if cid[i] < c1 and rid[i] < r1:
+            r = (rid[i] + 1) * ncols + 1 + cid[i]
+            if mask[r]:
+                r = find(ids, r)
+                sp_n[r] = 1
+    return sp_n
+
+
+@njit
+def find(array, item):
+    for idx, val in np.ndenumerate(array):
+        if val == item:
+            return idx[0]
+    return -1
+
+
+def _lazybuilder(props, index, ids, criterion):
+    for i in range(len(ids)):
+        neighbors = _getneighbor(
+            ids[i],
+            *props["nrc"],
+            props["rid"],
+            props["cid"],
+            ids,
+            props["mask"],
+            criterion,
+        )
+        yield index[i], neighbors
